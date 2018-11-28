2e3c5c7ec460959ba9ad848c85475ae7d9613ab1
moonshot123 <whwjddn200@naver.com>
2018-11-28 20:54:14 +0900

    2-commit

--- ex4/app8.js
+++ ex4/app8.js
@@ -18,7 +18,7 @@ let pool = mysql.createPool({
     host : 'localhost',
     user : 'root',
     password : '1234',
-    database : 'ex_mysql',
+    database : 'test',
     debug : false
 });
 
@@ -31,7 +31,11 @@ app.use(bodyParser.json());
 
 app.use('/public' , static(path.join(__dirname,'public')));
 
+app.use('/', router);
 
+
+
+// 회원가입(query)
 const addUser = function (id, name, age, password,callback) {
     console.log('addUser 호출됨');
 
@@ -47,7 +51,7 @@ const addUser = function (id, name, age, password,callback) {
 
         let data = {id:id, name:name, age:age, password:password}
 
-        let exec = conn.query('insert into test.user set ?', data, function (err,result) {
+        let exec = conn.query('insert into user set ?', data, function (err,result) {
             conn.release();
             console.log('실행대상 sql :'+exec.sql);
 
@@ -65,6 +69,44 @@ const addUser = function (id, name, age, password,callback) {
 
 
 
+
+//사용자인증(query)
+let authUser = function (id,password,callback) {
+    console.log('authUser 호출됨');
+
+    pool.getConnection(function (err,conn) {
+        if(err){
+            if(conn){
+                conn.release();
+            }
+            callback(err,null);
+            return;
+        }//if(err)
+            console.log('데이터 베이스 연결 스레드 아이디: '+ conn.threadId);
+            
+            let columns =['id','name','age'];
+            let tablename ='user';
+
+            
+            let exec = conn.query('select ?? from ?? where id = ? and password = ?',[columns, tablename, id ,password], function(err,rows){
+                conn.release();
+                console.log('실행대상 sql :'+ exec.sql);
+
+
+                if(rows.length > 0){
+                    console.log('아이디 [%s],패스워드 [%s] 가 일치하는 사용자 찾음.',id,password);
+                    callback(null,rows);
+                }else{
+                    console.log("일치하는 사용자를 찾지 못함");
+                    callback(null,null);
+                }
+            });//exec
+        
+    })// pool.getConnection
+}//authUser
+
+
+//사용자 등록
 router.route('/process/adduser').post(function (req, res) {
     console.log('/process/adduser 호출됨 ');
 
@@ -116,12 +158,71 @@ router.route('/process/adduser').post(function (req, res) {
 
 });
 
-//app.use('*', function (req,res) {
-//    res.status(404).send('<h1>ERROR- 페이지를 찾을수 없습니다.</h1>');
-//})
 
 
-//app.use('/', router);
+//사용자 로그인
+router.route('/process/login').post(function (req,res) {
+    console.log('/process/login 호출됨');
+
+    let paramID = req.body.id || req.query.id
+    let paramPassword = req.body.password || req.query.password
+    
+    console.log('요청 파라미터 : '+ paramID + ', '+ paramPassword );
+
+    if(pool){
+        authUser(paramID, paramPassword, function (err,rows) {
+            if(err){
+                console.log('======================= login fail =======================')
+                console.log('사용자 로그인 중 오류 발생 :'+err.stack);
+                res.writeHead("200",{'Content-Type':'text/html;Charset=utf-8'});
+                res.write('<h2> 사용자 로그인 중 오류발생 </h2>');
+                res.write('<p>' + err.stack + '</p>');
+                res.end();
+
+                return;
+            }//if(err)
+
+            if(rows){
+                console.log('======================= login success =======================')
+                console.log(rows);
+
+                let username = rows[0].name;
+                
+                res.writeHead("200",{'Content-Type':'text/html;Charset=utf-8'});
+                res.write('<h2> 사용자 로그인 성공</h2>');
+                res.write('<div><p> 사용자 아이디 :' + paramID + '</p></div>');
+                res.write('<div><p> 사용자 이름 :' + username + '</p></div>');
+                res.write('<br><br><a href="/public/login2.html"> 다시 로그인 하기 </a>');
+                //res.write('<br><br><a href="/public/login2.html/?id='+paramID+',name='+username+'"> 아이디 수정하기 </a>');
+                res.end();
+            }
+        })//authUser
+    }//if(pool)
+    
+})//router
+
+
+
+//사용자 수정
+//웹브라우저 클라이언트 >> express 웹서버 >> 컨트롤러(라우팅모델) >> 모델(mysql모델) >> 뷰(뷰엔진 ==== 이게 뷰템플릿) >> express 웹서버 >>>>>> 계속
+
+// node 에서 view에 데이터를 넣기 위해서는 일단 ejs형태로 해야됨, 일반적인 방식으로는 뷰에서 데이터를 넣어줄수 없고 
+// 이제껏 라우터에서 일반적으로 데이터를 바로 보여주는 방법으로만 사용하다보니 결과 응답을 
+
+// ejs 파일을 사용하는 것이 일반적인 방법인것 같다....... 동적 페이지를 관리하기 위해서는 기존의 html만으로는 안되는것 같다
+
+
+
+
+
+
+
+app.use('*', function (req,res) {
+    res.status(404).send('<h1>ERROR- 페이지를 찾을수 없습니다.</h1>');
+})
+
+
+
 
 http.createServer(app).listen(3000, function () {
     console.log('express 서버가 3000번 포트에서 시작됨');    
